b45348167b5763cdef1dbe322d6765e928f748d1
rohitw3code <rohitcode005@gmail.com>
2023-06-03 20:36:48 +0530

    label bug fixed

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import librosa.display
 model = keras.models.load_model('./model/speech_model.h5')
 
 # Categories (Emotion category)
-labels = ['fear', 'angry', 'disgust', 'neutral', 'sad', 'ps', 'happy']
+labels = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'ps', 'sad']
 
 # Initializing Flask
 app = Flask(__name__, template_folder='templates', static_folder='staticFiles')
