d524730c85e35c721e0d4806389e244fc2b415a5
rohitw3code <rohitcode005@gmail.com>
2023-06-03 20:36:38 +0530

    fixed label bug

--- app.py
+++ app.py
@@ -5,22 +5,29 @@ from flask import Flask, render_template, request, redirect, url_for
 import librosa
 import librosa.display
 
+# loading the Trained Model
+model = keras.models.load_model('./model/speech_model.h5')
 
-model = keras.models.load_model('./model//speech_model.h5')
+# Categories (Emotion category)
 labels = ['fear', 'angry', 'disgust', 'neutral', 'sad', 'ps', 'happy']
 
+# Initializing Flask
 app = Flask(__name__, template_folder='templates', static_folder='staticFiles')
 
+
+# This function is loading the audio and extracting the data out of it
 def extract_mfcc(filename):
      y, sr = librosa.load(filename, duration=3, offset=0.5)
      mfcc = np.mean(librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40).T, axis=0)
      return mfcc
 
 
-
+# to handel username and password for Login and Signup
 def write_to_file(username, password):
     with open('users.txt', 'a') as file:
         file.write(f'{username}:{password}\n')
+
+# to check if user already exist
 
 def check_credentials(username, password):
     with open('users.txt', 'r') as file:
@@ -32,12 +39,13 @@ def check_credentials(username, password):
 
 
 
+# Home page rendering
 @app.route("/home")
 def home():
     return render_template("home.html")
 
 
-
+# Signup Page rendering
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
@@ -48,7 +56,7 @@ def signup():
     return render_template('signup.html')
 
 
-
+# Login page rendering
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -62,6 +70,8 @@ def login():
 
 
 
+
+# This is used to handel prediction 
 @app.route('/predict2', methods=['GET', 'POST'])
 def upload_file1():
     if request.method == 'POST':
@@ -73,8 +83,8 @@ def upload_file1():
         pred_X = [x for x in pred_X_mfcc]
         pred_X = np.array(pred_X)
         pred_X = np.expand_dims(pred_X, -1)
-        pred = labels[model.predict(pred_X)[0].argmax()]
-        return render_template('reaction.html',mode=str(pred))
+        pred = labels[model.predict(pred_X)[0].argmax()] # this is used to predict the audio emotion
+        return render_template('reaction.html',mode=str(pred)) # sending the prediction value to reaction page
 
 
 if __name__ == "__main__":
